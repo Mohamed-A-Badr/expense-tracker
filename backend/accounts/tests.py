9009cf3a562891565e28f352a8452aabda6e74a6
Mohamed-A-Badr <badr49960@gmail.com>
2025-01-19 03:05:12 +0200

    Add test cases

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,3 +1,65 @@
 from django.test import TestCase
+from .models import CustomUser
+from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse
+from rest_framework import status
+
 
 # Create your tests here.
+class CustomUserModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = CustomUser.objects.create_user(
+            username="testuser", email="test@example.com", password="testpassword"
+        )
+
+    def test_user_object(self):
+        self.assertEqual(self.user.username, "testuser")
+        self.assertEqual(self.user.email, "test@example.com")
+        self.assertTrue(self.user.check_password("testpassword"))
+        self.assertEqual(str(self.user), "testuser and test@example.com")
+
+
+class RegisterLoginViewTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = CustomUser.objects.create_user(
+            username="testuser", email="test@example.com", password="testpassword"
+        )
+
+    def setUp(self):
+        response = self.client.post(
+            reverse("login"),
+            data={"email": "test@example.com", "password": "testpassword"},
+        )
+        self.refresh_token = response.data["refresh"]
+
+    def test_register_view(self):
+        response = self.client.post(
+            reverse("register"),
+            data={
+                "first_name": "new",
+                "last_name": "user",
+                "email": "user@mail.com",
+                "username": "new-user",
+                "password": "user123456",
+            },
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json(), {"message": "User created successfully"})
+
+    def test_login_view(self):
+        response = self.client.post(
+            reverse("login"),
+            data={"email": "test@example.com", "password": "testpassword"},
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("access", response.data)
+        self.assertIn("refresh", response.data)
+
+        response = self.client.post(
+            reverse("token_refresh"),
+            data={"refresh": self.refresh_token},
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("access", response.data)
--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from . import views
 
 urlpatterns = [
-    path("signup/", views.register_new_user, name="register_new_user"),
-    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("signup/", views.register_new_user, name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
